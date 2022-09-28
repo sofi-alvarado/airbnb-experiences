baa5bf2b56a75004f9ad890965cf470c399e48e5
Sofía Alvarado <alvaradosofiaH@outlook.com>
2022-09-27 23:50:09 -0600

    Adding Navbar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/NavBar.css';
+import Logo from '../images/imgLogo.png';
+
+function NavBar() {
+    return (
+        <nav className="navbar">
+           <img className="navbar-logo" src={Logo} alt="airbnb logo"/>
+        </nav>
+    )
+}
+
+export default NavBar;
