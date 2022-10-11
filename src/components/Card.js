09997f7e250c63e8dde90b12b195a19bcae8d34b
Sofía Alvarado <alvaradosofiaH@outlook.com>
2022-10-10 22:52:45 -0600

    Adding a badge for the card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,8 +4,22 @@ import imgStar from '../images/imgStar.png';
 import '../styles/styles.css';
 
 export default function Card(props) {
+    // Displaying Sold out badge
+
+    let badgeText // Declaring a variable to store the value of the badge
+
+    if (props.openSpots === 0) { // If the openSpots for the experience is equals to zero then
+        badgeText = "SOLD OUT" // the text in the badge will be "SOLD OUT"
+    } else if (props.location === "Online") { // Otherwise if the location is Online
+        badgeText = "ONLINE" //the text in the badge will be "ONLINE"
+    }
+
     return (
         <div className="card">
+                
+                {badgeText && <div className="card--badge">{badgeText}</div>}
+                {/*If our variable badgeText has a value it will render the div with our badge*/} 
+                
                 <img src={props.img} className="card--image" alt="card" />
             <div className="card--stats">
                 <img src={imgStar} className="card--star" alt="star" />
